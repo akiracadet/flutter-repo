0fd1e69bc935098e222be5b16f277a54579a95ca
Mavis Vermillion <mavis.vermillion.ichi@gmail.com>
2020-08-24 08:00:44 +0800

    Finished Widget Basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import './section/002/quiz_app/001/app.dart';
+import './sandbox/app.dart';
+// import './section/002/quiz_app/006/app.dart';
 
 void main() {
   runApp(MyApp());
 }
-
---
+++ lib/section/002/quiz_app/005/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
+  }
+}

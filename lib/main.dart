7d8f96ebf0a4269aa09420f0f19a07dd0a574068
Mavis Vermillion <mavis.vermillion.ichi@gmail.com>
2020-08-25 11:46:23 +0800

    Finished Stateful & Stateless Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './sandbox/app.dart';
-// import './section/002/quiz_app/006/app.dart';
+// import './section/002/quiz_app/010/app.dart';
 
 void main() {
   runApp(MyApp());
---
+++ lib/section/002/quiz_app/009/app.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex++;
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => {
+                // ...
+                print('Answer 3 chosen!')
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
